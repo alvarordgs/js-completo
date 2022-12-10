5539989d3c6ac8699b1abaa8f84ce0e76f928abc
Álvaro Rodrigues <alvarordgs98@gmail.com>
2022-12-10 23:01:56 +0100

    estudo das secoes de scroll suave e animacao do scroll

---
+++ scroll-suave/script.js
@@ -0,0 +1,27 @@
+function smoothScroll() {
+    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+    function scrollSuave(event) {
+        event.preventDefault();
+        const href = event.currentTarget.getAttribute("href");
+        const section = document.querySelector(href);
+
+        section.scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+        });
+
+        //forma alternativa
+        // const topo = section.offsetTop;
+        // window.scrollTo({
+        //     top: topo,
+        //     behavior: "smooth",
+        // });
+    }
+
+    linksInternos.forEach((link) => {
+        link.addEventListener("click", scrollSuave);
+    });
+}
+
+smoothScroll();
