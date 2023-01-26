24a8715c68fd205417f9474dda0c14e5b9766895
Álvaro Rodrigues <alvarordgs98@gmail.com>
2023-01-26 20:45:08 -0300

    estudo dos capitulos de dataset e modules

---
+++ efeitos-no-dom/modules-introducao/script.js
@@ -0,0 +1,9 @@
+import initTabNav from "./modules/tabnav.js";
+import initAccordion from "./modules/accordion.js";
+import initScrollSuave from "./modules/scroll-suave.js";
+import initAnimacaoScroll from "./modules/scroll-animation.js";
+
+initTabNav();
+initAccordion();
+initScrollSuave();
+initAnimacaoScroll();
